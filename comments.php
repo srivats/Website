09b87f45205b2002ba24014924090d74fb8b0a84
thiru <mc.thiru@gmail.comssh-keygen-trsa-C mc.thiru@gmail.com>
2012-08-11 10:07:36 +0530

    <p> tag added in comment form

--- comments.php
+++ comments.php
@@ -77,20 +77,30 @@
     <?php if (get_option('comment_registration') && !is_user_logged_in()) { ?>
       <p><?php printf(__('You must be <a href="%s">logged in</a> to post a comment.', 'roots'), wp_login_url(get_permalink())); ?></p>
     <?php } else { ?>
+    <!-- Added paragraph tag for label and input -->
       <form action="<?php echo get_option('siteurl'); ?>/wp-comments-post.php" method="post" id="commentform">
         <?php if (is_user_logged_in()) { ?>
           <p><?php printf(__('Logged in as <a href="%s/wp-admin/profile.php">%s</a>.', 'roots'), get_option('siteurl'), $user_identity); ?> <a href="<?php echo wp_logout_url(get_permalink()); ?>" title="<?php __('Log out of this account', 'roots'); ?>"><?php _e('Log out &raquo;', 'roots'); ?></a></p>
         <?php } else { ?>
-          <label for="author"><?php _e('Name', 'roots'); if ($req) _e(' (required)', 'roots'); ?></label>
-          <input type="text" class="text" name="author" id="author" value="<?php echo esc_attr($comment_author); ?>" size="22" tabindex="1" <?php if ($req) echo "aria-required='true'"; ?>>
-          <label for="email"><?php _e('Email (will not be published)', 'roots'); if ($req) _e(' (required)', 'roots'); ?></label>
-          <input type="email" class="text" name="email" id="email" value="<?php echo esc_attr($comment_author_email); ?>" size="22" tabindex="2" <?php if ($req) echo "aria-required='true'"; ?>>
+        <p>
+          <label for="author"><?php _e('Name', 'roots'); if ($req) _e(' *', 'roots'); ?></label>
+          <input type="text" class="text span2" name="author" id="author" value="<?php echo esc_attr($comment_author); ?>" tabindex="1" <?php if ($req) echo "aria-required='true'"; ?>>
+        </p>
+        <p>
+          <label for="email"><?php _e('Email', 'roots'); if ($req) _e(' *', 'roots'); ?></label>
+          <input type="email" class="text span2" name="email" id="email" value="<?php echo esc_attr($comment_author_email); ?>" tabindex="2" <?php if ($req) echo "aria-required='true'"; ?>>
+        </p>
+        <p>  
           <label for="url"><?php _e('Website', 'roots'); ?></label>
-          <input type="url" class="text" name="url" id="url" value="<?php echo esc_attr($comment_author_url); ?>" size="22" tabindex="3">
+          <input type="url" class="text span2" name="url" id="url" value="<?php echo esc_attr($comment_author_url); ?>"  tabindex="3">
         <?php } ?>
+      </p>
+      <p>
         <label for="comment"><?php _e('Comment', 'roots'); ?></label>
         <textarea name="comment" id="comment" class="input-xlarge" tabindex="4"></textarea>
-        <p><input name="submit" class="btn btn-primary" type="submit" id="submit" tabindex="5" value="<?php _e('Submit Comment', 'roots'); ?>"></p>
+      </p>
+      <p class="clearer"></p>
+        <div id="sub-cmt"><input name="submit" class="btn btn-primary" type="submit" id="submit" tabindex="5" value="<?php _e('Submit Comment', 'roots'); ?>"></div>
         <?php comment_id_fields(); ?>
         <?php do_action('comment_form', $post->ID); ?>
       </form>
