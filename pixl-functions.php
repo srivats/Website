b9e8a6ceb3bd4cb7e66858de9557f23107cbed7c
doofenshmirtz <blackcaps16@gmail.com>
2012-08-11 08:03:37 +0530

    removed tincymce "code" button

--- pixl-functions.php
+++ pixl-functions.php
@@ -97,30 +97,6 @@ function display_testimonials() {
   return $testimonials_slider;
 }
 
-
-add_action('init','add_code_button');
-
-function add_code_button() {
-  if( !current_user_can('edit_posts') && !current_user_can('edit_pages')) {
-    return;
-  }
-    if ( get_user_option('rich_editing') == 'true') {
-    add_filter('mce_external_plugins','add_code_plugin');
-    add_filter('mce_buttons','register_code_button');
-  }
-  
-}
-
-function register_code_button($buttons) {
-  array_push($buttons,"separator", 'code');
-  return $buttons;
-}
-
-function add_code_plugin($plugin_array) {
-  $plugin_array['code'] = get_template_directory_uri().'/js/code_plugin.js';
-  return $plugin_array;
-}
-
 function pixl_code( $atts,$content = null) {
   extract(shortcode_atts(array(
       'class' => $atts['class']
