a1467e45341803a32d858ebb0dbd845db32db428
blakcaps <blackcaps16@gmail.com>
2012-09-22 17:03:00 +0530

    Added comments list

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -2,7 +2,7 @@
 
 // Return post entry meta information
 function roots_entry_meta() {
-  echo '<i class="icon-time"></i><time class="updated" datetime="'. get_the_time('c') .'" pubdate>'. sprintf(__('Posted on %s at %s.', 'roots'), get_the_date(), get_the_time()) .'</time>';
+  echo '<i class="icon-calendar pull-left"></i><time class="updated pull-left" datetime="'. get_the_time('c') .'" pubdate>'. sprintf(__('Posted on %s at %s.', 'roots'), get_the_date(), get_the_time()) .'</time><span class="pull-right">' .comments_number( 'No comments', 'One comment', '% comment' ) .'</span>';
 
   //echo '<p class="byline author vcard">'. __('Written by', 'roots') .' <a href="'. get_author_posts_url(get_the_author_meta('ID')) .'" rel="author" class="fn">'. get_the_author() .'</a></p>';
 }
